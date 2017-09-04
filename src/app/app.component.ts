a6a3c3d0647edbb867a2243dd3b9ab2c57ee8553
Maximilian Röll <maximilian.roell@plentymarkets.com>
2017-09-04 16:24:53 +0200

    changed: names of base files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import {
 
 @Component({
                selector: 'plugin-terra-basic-app',
-               template: require('./plugin-terra-basic.component.html'),
-               styles:   [require('./plugin-terra-basic.component.scss')],
+               template: require('./app.component.html'),
+               styles:   [require('./app.component.scss')],
            })
 export class PluginTerraBasicComponent extends Translation
 {
