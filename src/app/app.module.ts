cf38104383fb98c164c479606ea99ea383099ac5
Maximilian Röll <maximilian.roell@plentymarkets.com>
2017-09-04 17:52:29 +0200

    added: Sidebar components created class sidebarComponents ....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpModule } from '@angular/http';
 import { TestComponent } from './test/test.component';
 import { TranslationModule } from 'angular-l10n';
 import { FormsModule } from '@angular/forms';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
               imports:      [
@@ -19,7 +20,8 @@ import { FormsModule } from '@angular/forms';
               declarations: [
                   AppComponent,
                   StartComponent,
-                  TestComponent
+                  TestComponent,
+                  SidebarComponent
               ],
               providers:    [],
               bootstrap:    [
