213f4d7dd7394d2069a17ed204d4442977def590
Max Hellige <max.hellige@plentymarkets.com>
2017-09-05 17:25:08 +0200

    add static Data

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,11 +9,32 @@ import {
         templateUrl: './sidebar.component.html',
         styleUrls: ['./sidebar.component.scss']
     })
-export class SidebarComponent implements OnInit
-{
-    ngOnInit()
-    {
+export class SidebarComponent implements OnInit {
+    ngOnInit() {
 
     }
+
+    compArray = [
+        {
+            buttons: [
+                {
+                    id: 0,
+                    name: 'Terra-Button'
+                },
+                {
+                    id: 1,
+                    name: 'Terra-Button'
+                },
+            ]
+        },
+        {
+            navigation: [
+                {
+                    id: 2,
+                    name: 'Terra-Navigator'
+                },
+            ]
+        }
+    ];
 
 }
