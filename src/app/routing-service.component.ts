4c0f8d68a348a6567a354c8cf421f60b4aeb89fd
Max Hellige <max.hellige@plentymarkets.com>
2017-09-08 16:17:03 +0200

    no message

--- src/app/routing-service.component.ts
+++ src/app/routing-service.component.ts
@@ -1,21 +1,21 @@
-import {Injectable} from '@angular/core';
-import {Router} from "@angular/router";
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
 
-import {MainviewComponent} from "./mainview/mainview.component";
-import {OverviewComponent} from "./templates/overview/overview.template";
-import {ExampleComponent} from "./templates/example/example.template";
-import {ApiComponent} from "./templates/api/api.template";
+import { MainviewComponent } from "./mainview/mainview.component";
+import { OverviewComponent } from "./templates/overview/overview.template";
+import { ExampleComponent } from "./templates/example/example.template";
+import { ApiComponent } from "./templates/api/api.template";
 
 @Injectable()
 export class RoutingService {
 
-    constructor(private router:Router) { }
+    constructor( private router:Router ) { }
 
-    createRoutes(compArray:Array<any>):void
+    createRoutes( compArray:Array<any> ) :void
     {
         let routeArray = [];
 
-        for(let data of compArray) {
+        for( let data of compArray ) {
            let objData =  {
                 path:data.name,
                 component: MainviewComponent,
