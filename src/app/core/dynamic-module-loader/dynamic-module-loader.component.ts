ff20aafd820e7c4542d0ad927e2f171cda890724
Max Hellige <max.hellige@plentymarkets.com>
2017-09-26 09:37:24 +0200

    Add TabMenu functions to Mainview -> Examples

--- src/app/core/dynamic-module-loader/dynamic-module-loader.component.ts
+++ src/app/core/dynamic-module-loader/dynamic-module-loader.component.ts
@@ -7,35 +7,56 @@ import {
     OnDestroy,
     ViewChild,
     ViewContainerRef,
+    OnInit
 } from '@angular/core';
 import {
     ActivatedRoute,
     Data
 } from '@angular/router';
-import { JitCompiler } from '@angular/compiler';
-import { TranslationService } from 'angular-l10n';
+import {JitCompiler} from '@angular/compiler';
+import {TranslationService} from 'angular-l10n';
+import {Http} from '@angular/http';
 
 @Component({
     selector: 'dynamic-module-loader',
-    template: require('./dynamic-module-loader.component.html')
+    template: require('./dynamic-module-loader.component.html'),
+    styleUrls: ['./dynamic-module-loader.component.scss']
 })
-export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy
-{
+export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy, OnInit {
     @ViewChild('viewChildTarget', {read: ViewContainerRef}) viewChildTarget;
 
-    private _moduleWithProviders:ModuleWithProviders;
-    private _componentRef:ComponentRef<any>;
+    private _moduleWithProviders: ModuleWithProviders;
+    private _componentRef: ComponentRef<any>;
 
-    constructor(private _jitCompiler:JitCompiler,
-                private _activatedRoute:ActivatedRoute,
-                public translation:TranslationService)
-    {
+    private _htmlCode: string;
+    private _cssCode: string;
+    private _typescriptCode: string;
+    private _temp: string;
+
+    constructor(private _jitCompiler: JitCompiler,
+                private _activatedRoute: ActivatedRoute,
+                public translation: TranslationService,
+                public http: Http) {
+        this._temp = '';
+
     }
 
-    ngAfterViewInit()
-    {
-        switch(this._activatedRoute.component['name'])
-        {
+    ngOnInit() {
+
+        this.http.get("assets/docu/examples/terra-button/terra-button.html").subscribe((res: any) => {
+            this._htmlCode = res.text();
+        });
+        this.http.get("assets/docu/examples/terra-button/terra-button.css").subscribe((res: any) => {
+            this._cssCode = res.text();
+        });
+        this.http.get("assets/docu/examples/terra-button/terra-button.ts").subscribe((res: any) => {
+            this._typescriptCode = res.text();
+        });
+
+    }
+
+    ngAfterViewInit() {
+        switch (this._activatedRoute.component['name']) {
             case 'DynamicPluginLoaderComponent':
                 this.loadComponentData(this._activatedRoute.data);
                 break;
@@ -44,38 +65,47 @@ export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy
         }
     }
 
-    ngOnDestroy()
-    {
-        if(this._componentRef)
-        {
+    ngOnDestroy() {
+        if (this._componentRef) {
             this._componentRef.destroy();
         }
     }
 
-    private loadComponentData(data:Data):void
-    {
-        data.subscribe((resolveData) =>
-        {
-            if(resolveData !== {})
-            {
+    private loadComponentData(data: Data): void {
+        data.subscribe((resolveData) => {
+            if (resolveData !== {}) {
                 this._moduleWithProviders = resolveData as ModuleWithProviders;
                 this.updateComponent();
             }
         });
     }
 
-    public updateComponent():void
-    {
+    public updateComponent(): void {
         this._jitCompiler.compileModuleAndAllComponentsAsync(this._moduleWithProviders.ngModule)
-            .then((moduleWithFactories:ModuleWithComponentFactories<any>) =>
-            {
-                moduleWithFactories.componentFactories.forEach((factory) =>
-                {
-                    if(factory.componentType.name === 'CustomDynamicComponent')
-                    {
+            .then((moduleWithFactories: ModuleWithComponentFactories<any>) => {
+                moduleWithFactories.componentFactories.forEach((factory) => {
+                    if (factory.componentType.name === 'CustomDynamicComponent') {
                         this._componentRef = this.viewChildTarget.createComponent(factory);
                     }
                 });
             });
     }
+
+    public showCode(codeType: string): void {
+        switch (codeType) {
+            case 'Html':
+                this._temp = this._htmlCode;
+                break;
+            case 'Css':
+                this._temp = this._cssCode;
+                break;
+            case 'Typescript':
+                this._temp = this._typescriptCode;
+                break;
+            default:
+                break;
+        }
+
+    }
+
 }
