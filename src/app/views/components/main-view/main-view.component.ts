cd0f61b65b1548ed723ec404c29c28da60fb6e24
Maximilian Röll <maximilian.roell@plentymarkets.com>
2017-11-09 17:49:20 +0100

    added: examples that got no html example only display api documentation

--- src/app/views/components/main-view/main-view.component.ts
+++ src/app/views/components/main-view/main-view.component.ts
@@ -38,8 +38,7 @@ export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy, O
     private _temp:string;
     private _isActive:boolean;
     private _isActiveButton:boolean;
-    private _buttonDisable:boolean;
-    private _codeIcon:string;
+    private _hideExample:boolean;
 
     private _htlmPath:string;
     private _cssPath:string;
@@ -69,11 +68,10 @@ export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy, O
         this._typescripPath = '';
         this._isActive = false;
         this._isActiveButton = false;
-        this._buttonDisable = false;
+        this._hideExample = false;
         this._tsHighlight = '';
         this._htmlHighlight = '';
         this._cssHighlight = '';
-        this._codeIcon = '';
 
         this._componentName = _activatedRoute.routeConfig.data.componentName;
         this._apiPath = _activatedRoute.routeConfig.data.apiPath;
@@ -91,7 +89,6 @@ export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy, O
         this._cssPath = this._activatedRoute.routeConfig.data.cssPath;
         this._typescripPath = this._activatedRoute.routeConfig.data.tsPath;
         this._componentName = this._activatedRoute.routeConfig.data.componentName;
-        this._codeIcon = '</>';
         this._alert.closeAlertByIdentifier('info');
 
         this.http.get(this._htlmPath).finally(() =>
@@ -107,22 +104,18 @@ export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy, O
 
         this.http.get(this._cssPath).finally(() =>
         {
-            this.checkTemplate(this._cssCode);
         }).subscribe((res:any) =>
         {
             this._cssCode = res.text();
             this._cssHighlight = `<pre><code class="css highlight">${this._cssCode}</code></pre>`;
-            this.checkTemplate(this._cssCode);
         });
 
         this.http.get(this._typescripPath).finally(() =>
         {
-            this.checkTemplate(this._typescriptCode);
         }).subscribe((res:any) =>
         {
             this._typescriptCode = res.text();
             this._tsHighlight = `<pre><code class="typescript highlight">${this._typescriptCode}</code></pre>`;
-            this.checkTemplate(this._typescriptCode);
         });
 
         this._overviewMarkDownPath = this._activatedRoute.routeConfig.data.OverviewMdPath;
@@ -154,13 +147,13 @@ export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy, O
 
     private checkTemplate(str:string):void
     {
-        if(str == '')
+        if(str.length === 0)
         {
-            this._buttonDisable = true;
+            this._hideExample = true;
         }
         else
         {
-            this._buttonDisable = false;
+            this._hideExample = false;
         }
     }
 
@@ -203,7 +196,7 @@ export class DynamicPluginLoaderComponent implements AfterViewInit, OnDestroy, O
 
     public showCodeMenu():void
     {
-        if(this._buttonDisable === false)
+        if(this._hideExample === false)
         {
             if(this._isActive === true)
             {
