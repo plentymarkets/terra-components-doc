f0504fc37deaaeb18ddfe554dd794e61122289c0
Max Hellige <max.hellige@plentymarkets.com>
2017-11-20 10:30:03 +0100

    blaaaa

--- src/app/views/components/component-view/component-view.component.ts
+++ src/app/views/components/component-view/component-view.component.ts
@@ -31,27 +31,24 @@ export class ComponentViewComponent implements AfterViewInit, OnDestroy, OnInit
     private _moduleWithProviders:ModuleWithProviders;
     private _componentRef:ComponentRef<any>;
 
-    private _htmlCode:string;
-    private _cssCode:string;
-    private _typescriptCode:string;
+    private _highlitedHtmlCode:string;
+    private _highlitedCssCode:string;
+    private _highlitedTsCode:string;
+    private _apiCode:any;
 
-    private _temp:string;
-    private _isActive:boolean;
-    private _isActiveButton:boolean;
-    private _hideExample:boolean;
-
-    private _htlmPath:string;
+    private _htmlPath:string;
     private _cssPath:string;
+    private _tsPath:string;
     private _apiPath:string;
-    private _typescripPath:string;
     private _componentName:string;
-    private _apiHtml:any;
     private _overviewMarkDownPath:string;
     private _isMarkDownPath:boolean;
 
-    private _tsHighlight:string;
-    private _htmlHighlight:string;
-    private _cssHighlight:string;
+    private _rawHtmlCode:string;
+    private _rawCssCode:string;
+    private _rawTsCode:string;
+
+    private _hideExample:boolean;
 
     private _alert:TerraAlertComponent = TerraAlertComponent.getInstance();
 
@@ -59,67 +56,60 @@ export class ComponentViewComponent implements AfterViewInit, OnDestroy, OnInit
                 private _activatedRoute:ActivatedRoute,
                 public http:Http)
     {
-        this._alert.closeAlertByIdentifier('info');
-        this._temp = '';
-        this._htlmPath = '';
+        this._htmlPath = '';
         this._cssPath = '';
-        this._htmlCode = '';
-        this._cssCode = '';
-        this._typescriptCode = '';
-        this._typescripPath = '';
-        this._isActive = false;
-        this._isActiveButton = false;
+        this._tsPath = '';
+        this._highlitedHtmlCode = '';
+        this._highlitedCssCode = '';
+        this._highlitedTsCode = '';
         this._hideExample = false;
-        this._tsHighlight = '';
-        this._htmlHighlight = '';
-        this._cssHighlight = '';
-
-        this._componentName = _activatedRoute.routeConfig.data.componentName;
-        this._apiPath = _activatedRoute.routeConfig.data.apiPath;
-
-        http.get(this._apiPath).subscribe((res:any) =>
-        {
-            this._apiHtml = res.text();
-        });
-
+        this._rawHtmlCode = '';
+        this._rawCssCode = '';
+        this._rawTsCode = '';
     }
 
     ngOnInit()
     {
-        this._htlmPath = this._activatedRoute.routeConfig.data.htmlPath;
-        this._cssPath = this._activatedRoute.routeConfig.data.cssPath;
-        this._typescripPath = this._activatedRoute.routeConfig.data.tsPath;
-        this._componentName = this._activatedRoute.routeConfig.data.componentName;
-        this._alert.closeAlertByIdentifier('info');
 
-        this.http.get(this._htlmPath).finally(() =>
+        this._alert.closeAlertByIdentifier('info');
+        this._htmlPath = this._activatedRoute.routeConfig.data.htmlPath;
+        this._cssPath = this._activatedRoute.routeConfig.data.cssPath;
+        this._tsPath = this._activatedRoute.routeConfig.data.tsPath;
+        this._apiPath = this._activatedRoute.routeConfig.data.apiPath;
+        this._componentName = this._activatedRoute.routeConfig.data.componentName;
+        this._overviewMarkDownPath = this._activatedRoute.routeConfig.data.OverviewMdPath;
+
+        this.http.get(this._apiPath).subscribe((res:any) =>
         {
-            this.checkTemplate(this._htmlCode);
+            this._apiCode = res.text();
+        });
+
+        this.http.get(this._htmlPath).finally(() =>
+        {
         }).subscribe((res:any) =>
         {
-            this._htmlCode = res.text();
-            this._htmlHighlight = this.htmlStringEscape(this._htmlCode);
-            this._htmlHighlight = `<pre><code class="xml highlight">${this._htmlHighlight}</code></pre>`;
-            this.checkTemplate(this._htmlCode);
+            this._rawHtmlCode = res.text();
+            this._highlitedHtmlCode = this.htmlStringEscape(this._rawHtmlCode);
+            this._highlitedHtmlCode = `<pre><code class="xml highlight">${this._highlitedHtmlCode}</code></pre>`;
+            this.checkTemplate(this._highlitedHtmlCode);
         });
 
         this.http.get(this._cssPath).finally(() =>
         {
         }).subscribe((res:any) =>
         {
-            this._cssCode = res.text();
-            this._cssHighlight = `<pre><code class="css highlight">${this._cssCode}</code></pre>`;
+            this._rawCssCode = res.text();
+            this._highlitedCssCode = `<pre><code class="css highlight">${this._rawCssCode}</code></pre>`;
         });
 
-        this.http.get(this._typescripPath).finally(() =>
+        this.http.get(this._tsPath).finally(() =>
         {
         }).subscribe((res:any) =>
         {
-            this._typescriptCode = res.text();
-            this._tsHighlight = `<pre><code class="typescript highlight">${this._typescriptCode}</code></pre>`;
+            this._rawTsCode = res.text();
+            this._highlitedTsCode = `<pre><code class="typescript highlight">${this._rawTsCode}</code></pre>`;
         });
 
-        this._overviewMarkDownPath = this._activatedRoute.routeConfig.data.OverviewMdPath;
         if(!isNullOrUndefined(this._overviewMarkDownPath))
         {
             this._isMarkDownPath = true;
@@ -148,7 +138,7 @@ export class ComponentViewComponent implements AfterViewInit, OnDestroy, OnInit
 
     private checkTemplate(str:string):void
     {
-        if(str.length === 0)
+        if(str.length == 0)
         {
             this._hideExample = true;
         }
@@ -193,40 +183,6 @@ export class ComponentViewComponent implements AfterViewInit, OnDestroy, OnInit
                     }
                 });
             });
-    }
-
-    public showCodeMenu():void
-    {
-        if(this._hideExample === false)
-        {
-            if(this._isActive === true)
-            {
-                this._isActive = false;
-            }
-            else
-            {
-                this._isActive = true;
-                this._temp = this._htmlHighlight;
-            }
-        }
-    }
-
-    public showCode(codeType:string):void
-    {
-        switch(codeType)
-        {
-            case 'Html':
-                this._temp = this._htmlHighlight;
-                break;
-            case 'Css':
-                this._temp = this._cssHighlight;
-                break;
-            case 'Typescript':
-                this._temp = this._tsHighlight;
-                break;
-            default:
-                break;
-        }
     }
 
     public copyText(text):void
