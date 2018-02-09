8489d2e543305e18c8197ffe1eda3844caf9c467
Maximilian Röll <maximilian.roell@plentymarkets.com>
2018-02-09 15:41:47 +0100

    ADDED: Icon live search. 	* searchbar with styles

--- src/app/views/icons/iconview/icon-view.component.ts
+++ src/app/views/icons/iconview/icon-view.component.ts
@@ -1,5 +1,6 @@
 import {
     Component,
+    OnChanges,
     OnInit
 } from '@angular/core';
 import { iconService } from '../service/icon.service';
@@ -8,14 +9,15 @@ import { TerraSuggestionBoxValueInterface } from '@plentymarkets/terra-component
 import { ScrollToViewHelper } from '../../../helper/scrollToView.helper';
 import { HighlightTextHelper } from '../../../helper/highlightText.helper';
 import { Observable } from 'rxjs/Observable';
+import { isNullOrUndefined } from 'util';
 
 @Component({
-    selector:    'iconview',
-    templateUrl: './icon-view.component.html',
-    styles:      [require('./icon-view.component.scss'),
-                  require('./icon-view.component.glob.scss').toString()
-    ]
-})
+               selector:    'iconview',
+               templateUrl: './icon-view.component.html',
+               styles:      [require('./icon-view.component.scss'),
+                             require('./icon-view.component.glob.scss').toString()
+               ]
+           })
 export class IconViewComponent implements OnInit
 {
 
@@ -25,7 +27,9 @@ export class IconViewComponent implements OnInit
     private _iconListCodeExample:string;
     private _iconListCodeExamplePath:string = 'assets/iconExample/iconListCodeExample.html';
     private _iconList:Array<TerraSuggestionBoxValueInterface> = [];
-    private _suggestionboxValue:any;
+    private _searchValue:string;
+    private _searchResultCounter: number;
+    private _searchResultText: string;
 
     constructor(private _data:iconService,
                 private _scrollToViewHelper:ScrollToViewHelper,
@@ -37,11 +41,53 @@ export class IconViewComponent implements OnInit
             this._iconButtonCodeExamplePath = 'src/app/assets/iconExample/iconButtonCodeExample.html';
             this._iconListCodeExamplePath = 'src/app/assets/iconExample/iconListCodeExample.html';
         }
-        this._suggestionboxValue = "";
-        this._iconList.push({
-            caption: "",
-            value:   ""
-        });
+    }
+
+    checkInput()
+    {
+        let searchValue = this._searchValue.toLowerCase();
+        this._searchResultCounter = 0;
+
+        if(!isNullOrUndefined(searchValue) && searchValue !== "")
+        {
+            this._iconList.forEach(icon => {
+                if(!isNullOrUndefined(icon.value) && icon.value !== "" )
+                {
+                    if(icon.value.includes(searchValue))
+                    {
+                        let iconContainer = document.getElementById(icon.value+'_container');
+                        iconContainer.style.display = "block";
+                        this._searchResultCounter++;
+                    }
+                    else
+                    {
+                        let iconContainer = document.getElementById(icon.value+'_container');
+                        iconContainer.style.display = "none";
+                    }
+                }
+
+            });
+        }
+        else
+        {
+            if(searchValue === "")
+            {
+                this._iconList.forEach( icon =>{
+                    let iconContainer = document.getElementById(icon.value+'_container');
+                    iconContainer.style.display = "block";
+                    this._searchResultCounter++;
+                });
+
+            }
+        }
+        if(this._searchResultCounter == 1)
+        {
+            this._searchResultText = " matching result";
+        }
+        else
+        {
+            this._searchResultText = " matching results";
+        }
     }
 
     ngOnInit()
@@ -49,20 +95,20 @@ export class IconViewComponent implements OnInit
         Observable.combineLatest(
             this.http.get(this._iconButtonCodeExamplePath),
             this.http.get(this._iconListCodeExamplePath),
-            (button:any, list:any) =>
-            {
+            (button:any, list:any) => {
                 return {
                     button: button.text(),
                     list:   list.text()
                 };
             }
-        ).subscribe((data:any) =>
-        {
+        ).subscribe((data:any) => {
             this._iconButtonCodeExample = this._highlightTextHelper.highlightText(data.button, 'xml');
             this._iconListCodeExample = this._highlightTextHelper.highlightText(data.list, 'xml');
         });
         this._newIconArray = this._data.loadIconArray();
         this.buildSuggestionBoxArray();
+        this._searchResultCounter = this._iconList.length;
+        this._searchResultText = " matching results";
     }
 
     buildSuggestionBoxArray():void
