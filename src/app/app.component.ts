733c648238ba93b541303357d419a50053f83655
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-07-11 16:45:11 +0200

    reduce code in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,9 @@
-import {
-    Component,
-    OnInit
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'tcd-app',
     templateUrl: './app.component.html',
     styleUrls:   ['./app.component.scss'],
 })
-export class AppComponent implements OnInit
-{
-    constructor()
-    {
-    }
-
-    ngOnInit()
-    {
-    }
-}
+export class AppComponent
+{}
