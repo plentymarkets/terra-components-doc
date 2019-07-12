f6cf5284dad621db46874d8a08705622e1864abd
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-07-12 09:34:00 +0200

    Add more components to the map

--- src/app/component-data.map.ts
+++ src/app/component-data.map.ts
@@ -4,5 +4,13 @@ export const componentMap:TerraKeyValueInterface<any> = {
     TerraAlertPanelComponent: {
         path: 'alert/example/terra-alert.component.example',
         group: 'Alert'
+    },
+    TerraButtonComponent: {
+        path: 'buttons/button/example/terra-button.component.example',
+        group: 'Button'
+    },
+    TerraToggleComponent: {
+        path: 'buttons/toggle/example/terra-toggle.component.example',
+        group: 'Button'
     }
 };
