a947c58de6099b44659d835699aff2901867e571
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-07-26 08:37:52 +0200

    format code of the spec

--- src/app/views/components/component-view-v2/component-view-v2.component.spec.ts
+++ src/app/views/components/component-view-v2/component-view-v2.component.spec.ts
@@ -1,25 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+    async,
+    ComponentFixture,
+    TestBed
+} from '@angular/core/testing';
 
 import { ComponentViewV2Component } from './component-view-v2.component';
 
-describe('ComponentViewV2Component', () => {
-  let component: ComponentViewV2Component;
-  let fixture: ComponentFixture<ComponentViewV2Component>;
+describe('ComponentViewV2Component', () =>
+{
+    let component:ComponentViewV2Component;
+    let fixture:ComponentFixture<ComponentViewV2Component>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ComponentViewV2Component ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() =>
+    {
+        TestBed.configureTestingModule({
+            declarations: [ComponentViewV2Component]
+        })
+               .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ComponentViewV2Component);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() =>
+    {
+        fixture = TestBed.createComponent(ComponentViewV2Component);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () =>
+    {
+        expect(component).toBeTruthy();
+    });
 });
