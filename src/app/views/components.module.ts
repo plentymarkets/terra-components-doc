c2135c484fc974e672a14595ce166cffb8f58a53
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-07-26 08:17:49 +0200

    delete old component-view-component

--- src/app/views/components.module.ts
+++ src/app/views/components.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
-import { ComponentViewComponent } from './component-view/component-view.component';
-import { ComponentTemplateComponent } from '../components/component-template.component';
-import { ComponentSidebarComponent } from '../components/sidebar/component-sidebar.component';
-import { ComponentViewV2Component } from './component-view-v2/component-view-v2.component';
 import {
     TerraComponentsExamplesModule,
     TerraComponentsModule
 } from '@plentymarkets/terra-components';
 import { TabsModule } from 'ngx-bootstrap';
-
 import { CommonModule } from '@angular/common';
 import { MarkdownModule } from 'ngx-markdown';
 import { HttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { HighlightModule } from 'ngx-highlightjs';
+
+import { ComponentTemplateComponent } from '../components/component-template.component';
+import { ComponentSidebarComponent } from '../components/sidebar/component-sidebar.component';
+import { ComponentViewV2Component } from './component-view-v2/component-view-v2.component';
 
 
 @NgModule({
@@ -28,12 +27,10 @@ import { HighlightModule } from 'ngx-highlightjs';
     ],
     exports:      [ComponentTemplateComponent],
     declarations: [
-        ComponentViewComponent,
         ComponentTemplateComponent,
         ComponentSidebarComponent,
-        ComponentViewV2Component],
-    providers:    [],
+        ComponentViewV2Component
+    ]
 })
 export class ComponentsModule
-{
-}
+{}
