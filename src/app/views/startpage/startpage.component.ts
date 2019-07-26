b6bf7fb7fc214914521fdeb990df2485d20abb43
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-07-26 08:22:44 +0200

    remove unused RouteResolver

--- src/app/views/startpage/startpage.component.ts
+++ src/app/views/startpage/startpage.component.ts
@@ -1,29 +1,19 @@
-import {
-    Component,
-    OnInit
-} from '@angular/core';
-import { RouteResolver } from '../../resolve/route.resolver';
+import { Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
 
 @Component({
-    selector:    'startpage',
+    selector:    'tcd-startpage',
     templateUrl: './startpage.component.html',
     styleUrls:   ['./startpage.component.scss']
 })
-export class StartpageComponent implements OnInit
+export class StartpageComponent
 {
-    private _changelogData:object;
     public npmImagePath:string;
     public gitImagePath:string;
     public projectPath:string;
 
-    constructor(private _routeResolver:RouteResolver)
+    constructor()
     {
-    }
-
-    ngOnInit()
-    {
-        this._changelogData = this._routeResolver.dataChangelog;
         this.npmImagePath = 'assets/images/Npm-logo.svg';
         this.gitImagePath = 'assets/images/GitHub-logo.svg';
         this.projectPath = 'assets/images/';
@@ -34,5 +24,4 @@ export class StartpageComponent implements OnInit
             this.projectPath = '/src/app/assets/images/';
         }
     }
-
 }
