d7c0a81e89f6df3492c0e7f96d04be69f3d54213
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-08-08 11:05:06 +0200

    reactive optimizations but keep the function names which we rely on

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = function(config, options) {
     const env = process.env.ENV = process.env.NODE_ENV = options && options.fileReplacements.length > 0 ? 'production' : 'development';
@@ -16,6 +17,12 @@ module.exports = function(config, options) {
         } else {
             config.plugins = [definePlugin];
         }
+    }
+
+    const terserPlugin = config.optimization.minimizer.find(minimizer => minimizer instanceof TerserPlugin);
+    if(terserPlugin && terserPlugin.options && terserPlugin.options.terserOptions)
+    {
+        terserPlugin.options.terserOptions.keep_fnames = true;
     }
 
     return config;
