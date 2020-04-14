5ad0e6d94120874480fa5e6a56467f95516be639
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-04-14 15:52:59 +0200

    update webpack-config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,6 @@ webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = function(config, options) {
-    const env = process.env.ENV = process.env.NODE_ENV = options && options.fileReplacements.length > 0 ? 'production' : 'development';
-
-    // console.log('Environment: ' + env);
-
-    if(config && config.plugins) {
-        const definePlugin = new webpack.DefinePlugin({
-            'process.env': {
-                'ENV': JSON.stringify(env)
-            }
-        });
-        if (config.plugins) {
-            config.plugins.push(definePlugin);
-        } else {
-            config.plugins = [definePlugin];
-        }
-    }
-
     const terserPlugin = config.optimization.minimizer.find(minimizer => minimizer instanceof TerserPlugin);
     if(terserPlugin && terserPlugin.options && terserPlugin.options.terserOptions)
     {
